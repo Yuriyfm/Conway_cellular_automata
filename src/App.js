6dd6b1f3cfd3739525893cad00030825978dad41
Yuriy <yuriyfm@yandex.ru>
2022-11-05 21:24:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import CellsGrid from "./components/CellsGrid";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <div style={{minHeight: '90vh', width: '20vw', marginLeft: 20, float: 'left'}}>
+              <About/>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center', marginTop: 30}} className="App">
+              <CellsGrid/>
+          </div>
+      </div>
+
   );
 }
 
---
+++ src/components/CellsGrid.js
@@ -0,0 +1,114 @@
+import React, {useEffect, useState} from "react";
+
+
+let CellsGrid = () => {
+
+    let cellSize = 14;
+    let gridWidth = 50
+    let gridHeight = 50
+
+    let createCellsGrid = () => {
+        return Array(gridWidth).fill(0).map(row => new Array(gridHeight).fill(0))
+    }
+    let cellsGrid = createCellsGrid()
+    let [cells, setCells] = useState(cellsGrid)
+
+    let setCell = (x, y) => {
+        let newCells = cells
+        newCells[y][x] = 1
+        setCells([...newCells])
+    }
+    let deleteCell = (x, y) => {
+        let newCells = cells
+        newCells[y][x] = 0
+        setCells([...newCells])
+    }
+
+    let resetGrid = () => {
+        setCells(createCellsGrid())
+    }
+
+    let isSurvive = (y, x) => {
+        let livingCells = []
+        let rows;
+        let block;
+        if (y === 0) {
+            rows = cells.slice(y, y + 2)
+        } else if (y === cells.length -1) {
+            rows = cells.slice(y - 1, y + 1)
+        } else { rows = cells.slice(y - 1, y + 2)}
+
+        if (x === 0) {
+            block = rows.map(row => row.slice(x, x + 2))
+        } else if (y === cells.length -1) {
+            block = rows.map(row => row.slice(x - 1, x + 1))
+        } else { block = rows.map(row => row.slice(x - 1, x + 2))}
+
+        block.forEach(row => row.forEach(cell => cell === 1 && livingCells.push(cell)))
+        livingCells.splice(0, 1)
+        return livingCells.length === 2 || livingCells.length === 3;
+    }
+
+    let isBirth = (y, x) => {
+        let livingCells = []
+        let rows;
+        let block;
+        if (y === 0) {
+            rows = cells.slice(y, y + 2)
+        } else if (y === cells.length -1) {
+            rows = cells.slice(y - 1, y + 1)
+        } else { rows = cells.slice(y - 1, y + 2)}
+
+        if (x === 0) {
+            block = rows.map(row => row.slice(x, x + 2))
+        } else if (y === cells.length -1) {
+            block = rows.map(row => row.slice(x - 1, x + 1))
+        } else { block = rows.map(row => row.slice(x - 1, x + 2))}
+        block.forEach(row => row.forEach(cell => cell === 1 && livingCells.push(cell)))
+        return livingCells.length === 3
+    }
+
+
+    let startLifeProcess = () => {
+        let newCells = []
+        cells.forEach(row => newCells.push(row.slice(0)))
+        cells.forEach((row, y) => {
+            row.forEach((cell, x) => {
+                    if (cell === 0) {
+                        if (isBirth(y, x)) {
+                            newCells[y][x] = 1
+                        }
+                    } else if (cell === 1) {
+                        if (!isSurvive(y, x)) {
+                            console.log(isSurvive(y, x))
+                            newCells[y][x] = 0
+                        }
+                    }})})
+        setCells([...newCells])
+    }
+
+    return (
+        <div>
+            <div style={{margin: 10}}>
+                <button style={{marginRight: 20}} onClick={resetGrid}>обновить</button>
+                <button onClick={() => {
+                    startLifeProcess()
+                }}>старт</button>
+            </div>
+            {cells.map((row, y) => (
+                <div key={y} style={{display: 'flex'}}>
+                    {row.map((cell, x) => (
+                        <div key={x} style={{
+                            height: cellSize, width: cellSize,
+                            border: '1px solid lightgrey', backgroundColor: cell === 1 && 'red'
+                        }}
+                             onClick={() => cell === 1 ? deleteCell(x, y) : setCell(x, y)}
+                        >{''}</div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default CellsGrid;
