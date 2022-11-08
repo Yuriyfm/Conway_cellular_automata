ee501e78fa85d6e4d52d27f7968462fb2e06d861
Yuriy <yuriyfm@yandex.ru>
2022-11-08 19:50:26 +0600

    looping steps and speed control

--- src/components/CellsGrid.js
+++ src/components/CellsGrid.js
@@ -10,14 +10,25 @@ let CellsGrid = () => {
     let createCellsGrid = () => {
         return Array(gridWidth).fill(0).map(row => new Array(gridHeight).fill(0))
     }
-    let cellsGrid = createCellsGrid()
-    let [cells, setCells] = useState(cellsGrid)
+
+    let [cells, setCells] = useState(createCellsGrid())
+    let [isStartLife, setIsStartLife] = useState(false)
+    let [step, setStep] = useState(1000)
+
+    useEffect(() => {
+        if (isStartLife) {
+            let timerID = setInterval(() => {startLifeProcess()
+            }, step)
+            return () => clearTimeout(timerID)
+        }
+    }, [cells])
 
     let setCell = (x, y) => {
         let newCells = cells
         newCells[y][x] = 1
         setCells([...newCells])
     }
+
     let deleteCell = (x, y) => {
         let newCells = cells
         newCells[y][x] = 0
@@ -34,15 +45,19 @@ let CellsGrid = () => {
         let block;
         if (y === 0) {
             rows = cells.slice(y, y + 2)
-        } else if (y === cells.length -1) {
+        } else if (y === cells.length - 1) {
             rows = cells.slice(y - 1, y + 1)
-        } else { rows = cells.slice(y - 1, y + 2)}
+        } else {
+            rows = cells.slice(y - 1, y + 2)
+        }
 
         if (x === 0) {
             block = rows.map(row => row.slice(x, x + 2))
-        } else if (y === cells.length -1) {
+        } else if (y === cells.length - 1) {
             block = rows.map(row => row.slice(x - 1, x + 1))
-        } else { block = rows.map(row => row.slice(x - 1, x + 2))}
+        } else {
+            block = rows.map(row => row.slice(x - 1, x + 2))
+        }
 
         block.forEach(row => row.forEach(cell => cell === 1 && livingCells.push(cell)))
         livingCells.splice(0, 1)
@@ -55,45 +70,59 @@ let CellsGrid = () => {
         let block;
         if (y === 0) {
             rows = cells.slice(y, y + 2)
-        } else if (y === cells.length -1) {
+        } else if (y === cells.length - 1) {
             rows = cells.slice(y - 1, y + 1)
-        } else { rows = cells.slice(y - 1, y + 2)}
+        } else {
+            rows = cells.slice(y - 1, y + 2)
+        }
 
         if (x === 0) {
             block = rows.map(row => row.slice(x, x + 2))
-        } else if (y === cells.length -1) {
+        } else if (y === cells.length - 1) {
             block = rows.map(row => row.slice(x - 1, x + 1))
-        } else { block = rows.map(row => row.slice(x - 1, x + 2))}
+        } else {
+            block = rows.map(row => row.slice(x - 1, x + 2))
+        }
         block.forEach(row => row.forEach(cell => cell === 1 && livingCells.push(cell)))
         return livingCells.length === 3
     }
-
 
     let startLifeProcess = () => {
         let newCells = []
         cells.forEach(row => newCells.push(row.slice(0)))
         cells.forEach((row, y) => {
             row.forEach((cell, x) => {
-                    if (cell === 0) {
-                        if (isBirth(y, x)) {
-                            newCells[y][x] = 1
-                        }
-                    } else if (cell === 1) {
-                        if (!isSurvive(y, x)) {
-                            console.log(isSurvive(y, x))
-                            newCells[y][x] = 0
-                        }
-                    }})})
+                if (cell === 0) {
+                    if (isBirth(y, x)) {
+                        newCells[y][x] = 1
+                    }
+                } else if (cell === 1) {
+                    if (!isSurvive(y, x)) {
+                        newCells[y][x] = 0
+                    }
+                }
+            })
+        })
+        JSON.stringify(newCells) === JSON.stringify(cells) && setIsStartLife(false)
         setCells([...newCells])
     }
 
     return (
         <div>
-            <div style={{margin: 10}}>
-                <button style={{marginRight: 20}} onClick={resetGrid}>обновить</button>
+            <div style={{margin: 10, display: "flex", justifyContent: 'center'}}>
+                <div style={{marginRight: 20}}>
+                    <span style={{marginRight: 10}}>скорость шага (мс.)</span>
+                    <input type="number" onChange={(e) => setStep(+e.target.value)}/>
+                </div>
+                <button style={{marginRight: 20}} onClick={() => {
+                    resetGrid()
+                    setIsStartLife(false)
+                }}>обновить</button>
                 <button onClick={() => {
+                    setIsStartLife(true)
                     startLifeProcess()
-                }}>старт</button>
+                }}>старт
+                </button>
             </div>
             {cells.map((row, y) => (
                 <div key={y} style={{display: 'flex'}}>
